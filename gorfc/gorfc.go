a42980a7f112e71b40ed53cd7a06d42b44d3ff12
stubSAP <stubSAP>
2022-06-08 09:28:50 +0700

    feat: update close and call

--- gorfc/gorfc.go
+++ gorfc/gorfc.go
@@ -2,9 +2,17 @@ package gorfc
 
 type ConnectionParameters map[string]string
 
+func ConnectionFromParams(connectionParams ConnectionParameters) (conn *Connection, err error) {
+	return &Connection{}, nil
+}
+
 type Connection struct {
 }
 
-func ConnectionFromParams(connectionParams ConnectionParameters) (conn *Connection, err error) {
-	return &Connection{}, nil
+func (conn *Connection) Close() (err error) {
+	return nil
+}
+
+func (conn *Connection) Call(goFuncName string, params interface{}) (result map[string]interface{}, err error) {
+	return nil, nil
 }
